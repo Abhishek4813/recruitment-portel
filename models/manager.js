1ac3e4fe7fa2f0a7268314b3a8118e27f66d328c
abhishek <abhishekkumar0539@gmail.com>
2020-05-12 00:29:29 +0530

    schema created

---
+++ models/manager.js
@@ -0,0 +1,18 @@
+'use strict';
+const mongoose=require("mongoose");
+const managerschema=new mongoose.Schema({
+    employ_id:{
+      type:String,
+      unique:true,
+      index:true,
+    },
+    register_date:Date,
+    name:String,
+    email:String,
+    location:String,
+    jobs:[{type:String}],
+    freelance_recuriters:[{type:String}],
+    active_jobs:[{type:String}],
+});
+const managermodel=mongoose.model('manager',managerschema);
+module.exports=managermodel;
